bbe29702e886bee6fe890a34e8f60b39d7098e1f
Amie-dev <learn.amie01@gmail.com>
2026-01-13 23:10:47 +0530

    add logic for public and private Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router";
+import Auth from "./pages/auth/Auth";
+import Chat from "./pages/chat/Chat";
+import Home from "./pages/Home";
+import { Toaster } from "sonner";
+import { GuestRoute, PrivateRoute } from "./pages/PageGuards";
 
-const App:React.FC=()=> {
+const App: React.FC = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Routes>
+        {/* Guest-only routes */}
+        <Route element={<GuestRoute />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+        </Route>
+        {/* Authenticated-only routes */}
+        <Route element={<PrivateRoute />}>
+          <Route path="/chat" element={<Chat />} />
+        </Route>
+      </Routes>
 
-export default App
+      <Toaster />
+    </>
+  );
+};
+
+export default App;
