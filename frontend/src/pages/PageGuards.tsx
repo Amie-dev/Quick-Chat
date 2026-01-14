d6aad308dcfdfad5a55376f43756639bda24c034
Amie-dev <learn.amie01@gmail.com>
2026-01-14 19:57:54 +0530

    Update middleware that if in api /me router give value this time user access private route

--- frontend/src/pages/PageGuards.tsx
+++ frontend/src/pages/PageGuards.tsx
@@ -1,13 +1,34 @@
 import { Navigate, Outlet } from "react-router";
-import { useAuthStore } from "../stores/authStore";
+import { useAuth } from "../hooks/useAuth";
 
 export function PrivateRoute() {
-  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
-  return isAuthenticated ? <Outlet /> : <Navigate to="/auth" />;
+  const { data: user, isLoading, isError } = useAuth();
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError || !user) {
+    return <Navigate to="/auth" />;
+  }
+  return <Outlet />;
 }
 
 export function GuestRoute() {
-  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
-  return !isAuthenticated ? <Outlet /> : <Navigate to="/chat" />;
-}
+  const { data: user, isLoading} = useAuth();
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  // if (isError || !user) {
+  //   return <Navigate to="/auth" />;
+  // }
 
+  return !user ? <Outlet /> : <Navigate to="/chat" />;
+}
+// export function PrivateRoute() {
+//   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+//   return isAuthenticated ? <Outlet /> : <Navigate to="/auth" />;
+// }
+
+// export function GuestRoute() {
+//   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+//   return !isAuthenticated ? <Outlet /> : <Navigate to="/chat" />;
+// }
