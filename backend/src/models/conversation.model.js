8579f453b317ef982bf372a50499b45d1755798d
Amie-dev <learn.amie01@gmail.com>
2026-01-16 19:24:37 +0530

    add Conversation model

---
+++ backend/src/models/conversation.model.js
@@ -0,0 +1,46 @@
+import { Schema, model } from "mongoose";
+
+const conversationSchema = new Schema(
+  {
+    participants: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
+    lastMessage: {
+      type: Schema.Types.ObjectId,
+      ref: "Message",
+      index: true,
+    },
+    lastMessagePreview: {
+      content: String,
+      timestamp: Date, // fixed typo
+    },
+    unreadCounts: {
+      type: Map,
+      of: Number,
+      default: () => new Map(),
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+conversationSchema.index(
+  { "participants.0": 1, "participants.1": 1 },
+  { unique: true }
+);
+
+conversationSchema.pre("save", function (next) {
+  if (this.participants && this.participants.length === 2) {
+    this.participants = this.participants.map(p => p.toString()).sort();
+  }
+  next();
+});
+
+const Conversation = model("Conversation", conversationSchema);
+
+export default Conversation;
